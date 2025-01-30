5b8172be2a57c9a0ad42f2d593e9d51dee9b6453
No Name <dharmilkotak00@gmail.com>
2025-01-30 08:51:18 +0000

    Updated 2 files via CloudCannon

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import {CloudCannonConnect} from '@cloudcannon/react-connector'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
     <Navbar />
+    const AppComponent = CloudCannonConnect(Component);
    <Component {...pageProps} />
    <Footer />
    </>
